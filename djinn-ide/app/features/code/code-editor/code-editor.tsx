78f85f912104d5b7c8f1648a8a608ece874692fc
Belén Albeza <belen@hey.com>
2026-06-20 11:42:15 +0200

    :bug: Fix tab key (#67)
    * :bug: Fix tab key not indenting in the code editor
    
    * :bug: Fix tab key and identation levels in code editor

--- djinn-ide/app/features/code/code-editor/code-editor.tsx
+++ djinn-ide/app/features/code/code-editor/code-editor.tsx
@@ -3,7 +3,12 @@ import { useRef, useEffect } from "react";
 import { basicSetup } from "codemirror";
 import { EditorView, keymap } from "@codemirror/view";
 import { EditorState, Prec } from "@codemirror/state";
-import { defaultKeymap } from "@codemirror/commands";
+import {
+  defaultKeymap,
+  indentWithTab,
+  insertNewlineKeepIndent,
+} from "@codemirror/commands";
+import { indentUnit } from "@codemirror/language";
 
 import { useProjectStore } from "~/features/base/project.store";
 import { asm } from "../asm-lang/asm-lezer";
@@ -11,6 +16,7 @@ import { codeEditorTheme } from "./code-editor.theme";
 import { useEditorStore } from "./editor.store";
 import { errorMarkers, setErrors } from "./error-markers";
 
+// Custom keymaps that override the default setup
 const overridenShortcuts = Prec.highest(
   keymap.of([
     // Override the default Meta-Enter shortcut to run the project,
@@ -21,8 +27,12 @@ const overridenShortcuts = Prec.highest(
         return true; // consume the event
       },
     },
+    { key: "Enter", run: insertNewlineKeepIndent },
   ]),
 );
+
+// Custom keymaps that do not conflict with the default setup
+const customKeymap = [indentWithTab];
 
 export default function CodeEditor() {
   const editorRef = useRef<HTMLDivElement>(null);
@@ -37,7 +47,8 @@ export default function CodeEditor() {
         basicSetup,
         codeEditorTheme,
         overridenShortcuts,
-        keymap.of(defaultKeymap),
+        indentUnit.of("  "),
+        keymap.of([...customKeymap, ...defaultKeymap]),
         errorMarkers,
         asm(),
         EditorView.editorAttributes.of({
