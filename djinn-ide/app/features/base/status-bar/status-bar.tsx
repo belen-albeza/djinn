add89144cfe44c9baab82d25c6b9c65c4243abc2
Belén Albeza <belen@hey.com>
2026-06-21 00:08:45 +0200

    :tada: Display cursor position in the status bar

--- djinn-ide/app/features/base/status-bar/status-bar.tsx
+++ djinn-ide/app/features/base/status-bar/status-bar.tsx
@@ -1,17 +1,29 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 import { FloppyDiskIcon } from "@phosphor-icons/react";
+import type { Location } from "djinn-dev-wasm";
 
 import { useStatusBarStore } from "./status.store";
+import { useEditorStore } from "~/features/code/code-editor/editor.store";
 import MessageDisplay from "./message-display";
 import { cn } from "~/utils/cn";
 
-function SavedIndicator() {
+function SavedIndicator({ ...other }: React.SVGProps<SVGSVGElement>) {
   return (
     <FloppyDiskIcon
       size={20}
       weight="duotone"
       className={cn("opacity-0 animate-saved-flash")}
+      {...other}
     />
+  );
+}
+
+function Cursor({ position }: { position: Location }) {
+  return (
+    <span className="text-small text-ink tabular-nums">
+      Ln {position.line}, Col {position.column}
+    </span>
   );
 }
 
@@ -19,6 +31,11 @@ export default function StatusBar() {
   const savedTick = useStatusBarStore((state) => state.savedTick);
   const [savedFlashing, setSavedFlashing] = useState(false);
   const messages = useStatusBarStore((state) => state.messages);
+
+  // TODO: only show cursor position if we are in the code editor path (e.g. '/' or '/code')
+  const pathname = useLocation().pathname;
+  const shallShowCursor = pathname === "/" || pathname === "/code"; // TODO:
+  const cursor = useEditorStore((state) => state.cursor);
 
   useEffect(() => {
     if (savedTick > 0) {
@@ -30,7 +47,19 @@ export default function StatusBar() {
     <footer className="px-3 py-2 border-t border-sand-200 bg-sand-100 align-center">
       <p className="text-small flex direction-row gap-2 justify-between items-center">
         {<MessageDisplay messages={messages} />}
-        {savedFlashing && <SavedIndicator key={savedTick} />}
+        <span className="flex direction-row gap-2">
+          {savedFlashing && (
+            <SavedIndicator
+              key={savedTick}
+              onAnimationEnd={(e: React.AnimationEvent<SVGSVGElement>) => {
+                if (e.animationName === "saved-flash") {
+                  setSavedFlashing(false);
+                }
+              }}
+            />
+          )}
+          {shallShowCursor && <Cursor position={cursor} />}
+        </span>
       </p>
     </footer>
   );
