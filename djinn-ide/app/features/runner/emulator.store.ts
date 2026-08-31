0d6cbdae72c913d3df99104de644050e454f9d12
Belén Albeza <belen@hey.com>
2026-08-31 12:41:32 +0200

    :sparkles: Stop the emulator when the modal closes (#127)
    * :sparkles: Stop the emulator and free resources
    
    * :wrench: Update Rust job to cache builds

--- djinn-ide/app/features/runner/emulator.store.ts
+++ djinn-ide/app/features/runner/emulator.store.ts
@@ -3,14 +3,33 @@ import type { Emulator } from "djinn-dev-wasm";
 
 interface EmulatorStore {
   emulator: Emulator | null;
-  visible: boolean;
   setEmulator: (emulator: Emulator) => void;
+  visible: boolean;
   setVisible: (value: boolean) => void;
+  stop: (() => void) | null;
+  setStop: (stop: (() => void) | null) => void;
+  dispose: () => void;
 }
 
-export const useEmulatorStore = create<EmulatorStore>()((set) => ({
+export const useEmulatorStore = create<EmulatorStore>()((set, get) => ({
   emulator: null,
+  setEmulator: (emulator) => {
+    get().emulator?.free(); // make sure any previous emulator is freed
+    set({ emulator });
+  },
+
   visible: false,
-  setEmulator: (emulator) => set({ emulator }),
   setVisible: (value: boolean) => set({ visible: value }),
+
+  stop: null,
+  setStop: (stop) => set({ stop }),
+
+  dispose: () => {
+    const { emulator, stop } = get();
+    if (!emulator) return;
+
+    stop?.();
+    emulator.free();
+    set({ emulator: null, stop: null, visible: false });
+  },
 }));
